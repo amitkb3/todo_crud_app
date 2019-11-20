a63c0b9ffb2523c90590f74606e9632b6453b842
Amit Bothra <amit.bothra@outlook.com>
2019-11-19 19:12:08 -0600

    Updated View to display user inputed data via form

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -16,6 +16,21 @@ class Todo(db.Model):
     return f'<Todo {self.id} {self.description}>'
 
 db.create_all()
+
+# Route listening to route /todos/create which 
+# has information submited via the form
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+  # reading form data using imported request method in Flask
+  description = request.form.get('description', '')
+  # create new record in our todo table
+  # create todo object
+  todo = Todo(description=description)
+  db.session.add(todo)
+  db.session.commit()
+  # Render the view with new data
+  return redirect(url_for('index'))
 
 @app.route('/')
 def index():
